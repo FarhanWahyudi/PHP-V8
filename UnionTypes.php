541b4ea407e24266c50ffa1084738ca1f254dffa
farhan wahyudi <farhanwahyudi875@gmail.com>
2024-05-13 16:11:49 +0700

    Union Types

---
+++ UnionTypes.php
@@ -0,0 +1,11 @@
+<?php
+    class Example {
+        public string|int|array|bool $value;
+    }
+
+    $example = new Example();
+    $example->value = true;
+    $example->value = 'farhan';
+    $example->value = [];
+    $example->value = 200;
+?>
